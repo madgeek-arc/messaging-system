faf9f1b1549890a3fbe482ceddd1244c17059bce
Konstantinos Spyrou <spyroukon@gmail.com>
2023-04-11 16:29:44 +0300

    init commit

---
+++ src/test/java/gr/athenarc/messaging/MessagingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.athenarc.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
