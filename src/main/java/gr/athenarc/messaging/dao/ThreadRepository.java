b1c4b2f8dc65e32a8a66ff3d20f56d69e6c2c03c
Konstantinos Spyrou <spyroukon@gmail.com>
2023-04-27 14:12:04 +0300

    fixed import

--- src/main/java/gr/athenarc/messaging/dao/ThreadRepository.java
+++ src/main/java/gr/athenarc/messaging/dao/ThreadRepository.java
@@ -1,6 +1,9 @@
 package gr.athenarc.messaging.dao;
 
+import gr.athenarc.messaging.domain.Thread;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ThreadRepository extends MongoRepository<Thread, String> {
 }
