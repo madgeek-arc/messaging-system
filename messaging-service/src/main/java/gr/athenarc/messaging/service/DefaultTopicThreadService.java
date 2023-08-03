2fb969c48434917743b1cccae6fc70fed8149999
Konstantinos Spyrou <spyroukon@gmail.com>
2023-08-03 12:04:59 +0300

    removed email method and fixed setting message id

--- messaging-service/src/main/java/gr/athenarc/messaging/service/DefaultTopicThreadService.java
+++ messaging-service/src/main/java/gr/athenarc/messaging/service/DefaultTopicThreadService.java
@@ -1,11 +1,9 @@
 package gr.athenarc.messaging.service;
 
-import gr.athenarc.messaging.domain.Correspondent;
 import gr.athenarc.messaging.domain.Message;
 import gr.athenarc.messaging.domain.StoredMessage;
 import gr.athenarc.messaging.domain.TopicThread;
 import gr.athenarc.messaging.mailer.client.service.MailClient;
-import gr.athenarc.messaging.mailer.domain.EmailMessage;
 import gr.athenarc.messaging.repository.ReactiveMongoTopicThreadRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,10 +14,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class DefaultTopicThreadService implements TopicThreadService {
@@ -81,28 +75,14 @@ public class DefaultTopicThreadService implements TopicThreadService {
 
     @Override
     public Mono<TopicThread> addMessage(String threadId, Message message, boolean anonymousSender) {
-//        Set<String> groups = message.getTo().stream().map(Correspondent::getGroupId).filter(Objects::nonNull).collect(Collectors.toSet());
-//        groups.add(message.getFrom().getGroupId());
-//        Mono<List> mono = Mono.from(Flux.fromIterable(groups).flatMap(groupService::getUserEmails));
         return topicThreadRepository.findById(threadId).flatMap(
                         topic -> {
                             StoredMessage storedMessage = StoredMessage.of(message, anonymousSender);
+                            storedMessage.setId(String.valueOf(topic.getMessages().size()));
                             topic.getMessages().add(storedMessage);
-//                            sendEmails(topic, message, mono);
                             return topicThreadRepository.save(topic);
                         })
                 .log();
-    }
-
-    private void sendEmails(TopicThread topic, Message message, Mono<List> bcc) {
-        EmailMessage emailMessage = new EmailMessage();
-        emailMessage.setFrom("test.openaire@gmail.com");
-        emailMessage.setSubject(topic.getSubject());
-        emailMessage.setText(message.getBody());
-
-        emailMessage.setTo(message.getTo().stream().map(Correspondent::getEmail).collect(Collectors.toList()));
-        emailMessage.setBcc(bcc.block());
-        mailClient.sendMail(emailMessage);
     }
 
     @Override
