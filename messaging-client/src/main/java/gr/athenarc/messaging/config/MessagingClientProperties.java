e49ea6c70d9ff41bffed3565610de8dad8332f10
Konstantinos Spyrou <spyroukon@gmail.com>
2023-09-12 12:44:43 +0300

    Show warning if property is missing

--- messaging-client/src/main/java/gr/athenarc/messaging/config/MessagingClientProperties.java
+++ messaging-client/src/main/java/gr/athenarc/messaging/config/MessagingClientProperties.java
@@ -1,11 +1,18 @@
 package gr.athenarc.messaging.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
 
 @ConfigurationProperties(prefix = "messaging-system")
 public class MessagingClientProperties {
 
-    private Client client;
+    private static final Logger logger = LoggerFactory.getLogger(MessagingClientProperties.class);
+
+    private Client client = new Client();
 
     public Client getClient() {
         return client;
@@ -28,6 +35,13 @@ public class MessagingClientProperties {
 
         public void setEndpoint(String endpoint) {
             this.endpoint = endpoint;
+        }
+    }
+
+    @PostConstruct
+    void checkMissing() {
+        if (this.getClient() == null || !StringUtils.hasText(this.getClient().getEndpoint())) {
+            logger.warn("Missing property: 'messaging-system.client.endpoint'");
         }
     }
 }
